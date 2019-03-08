dfb33e8f53ca26fe7fa44871b1f6f0c1a0b3758c
Nikita <macbook@MacBook-Pro.local>
2019-03-08 15:09:06 +0300

    Устраняет ошибки и добавлять минимизированные файлы

---
+++ js/open-modal-minimized.js
@@ -0,0 +1,1 @@
+var buttonsearch = document.querySelector(".open-modal-button");var hotelsearch = document.querySelector(".hotel-search-inner");buttonsearch.addEventListener ("click",function(evt){evt.preventDefault();hotelsearch.classList.toggle ("form-invisible");});
